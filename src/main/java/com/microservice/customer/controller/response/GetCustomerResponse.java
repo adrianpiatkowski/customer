1287896a77cd9b57bae1d5c0ff8aa3abe1fae12d
Adrian <adrian17091996@gmail.com>
2020-11-25 15:13:35 +0100

    poprawki 2

--- src/main/java/com/microservice/customer/controller/response/GetCustomerResponse.java
+++ src/main/java/com/microservice/customer/controller/response/GetCustomerResponse.java
@@ -3,6 +3,7 @@ package com.microservice.customer.controller.response;
 import com.microservice.customer.dto.CustomerDto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
